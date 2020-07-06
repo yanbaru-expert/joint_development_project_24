b945493c1e6389885a6a956a130b4cd8864de15f
KOICHI HATANO <hatch.k53@gmail.com>
2020-07-07 08:13:24 +0900

    新規投稿機能 new, create を実装（ストロングパラメータを利用)

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,2 +1,22 @@
 class MessagesController < ApplicationController
+
+    def new
+         @message = Message.new
+    end
+
+    def create
+        @message = Message.create(message_params)
+    end
+
+    def index
+        @messages = Message.all
+    end
+
+
+    private
+
+    def message_params
+        params.require(:message).permit(:title, :content)
+    end
+
 end
