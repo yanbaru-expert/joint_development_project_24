a78338aa1cd5c0af8974e86212e46fb1d45e7e35
AKINORI NAKAHARA <railiminal.garnet@gmail.com>
2020-07-06 23:53:50 +0900

    一覧表示機能 index を実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,14 @@
 class UsersController < ApplicationController
+  def index
+    @users = User.all
+  end
   def new
     @user = User.new
   end
   def create
     User.create(user_params)
   end
-  
+
   private
 
   def user_params
