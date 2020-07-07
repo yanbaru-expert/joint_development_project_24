6ac6c21143f0eeac3244821a6cb0441be54d6457
KOICHI HATANO <hatch.k53@gmail.com>
2020-07-07 10:05:34 +0900

    削除機能 destroy を実装,updateアクション修正

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -19,6 +19,15 @@ class MessagesController < ApplicationController
     def edit
         @message = Message.find(params[:id])
     end
+    def update
+        message = Message.find(params[:id])
+        message.update(message_params)
+    end
+
+    def destroy
+        message = Message.find(params[:id])
+        message.delete
+    end
     private
 
     def message_params
