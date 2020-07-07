152c100cf4916c41b1c43a80fa2bba82c2d00427
n.isobe <ganmodoki7.43dari@gmail.com>
2020-07-07 11:56:38 +0900

    一覧表示機能 index を実装

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,11 +1,16 @@
 class TasksController < ApplicationController
 
+  def index
+    @tasks = Task.all
+  end
+
   def new
     @task = Task.new
   end
 
   def create
     @task = Task.create(task_params)
+    redirect_to tasks_path
   end
 
   private
