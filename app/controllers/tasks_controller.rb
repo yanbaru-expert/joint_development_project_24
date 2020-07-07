b5aaa0009ec80099d85a9e567598e7f2c76f5d00
n.isobe <ganmodoki7.43dari@gmail.com>
2020-07-07 12:21:20 +0900

    編集機能 edit, update を実装

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -9,12 +9,22 @@ class TasksController < ApplicationController
   end
 
   def create
-    @task = Task.create(task_params)
+    Task.create(task_params)
     redirect_to tasks_path
   end
 
   def show
     @task = Task.find(params[:id])
+  end
+
+  def edit 
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    task = Task.find(params[:id])
+    task.update(task_params)
+    redirect_to tasks_path
   end
 
   private
